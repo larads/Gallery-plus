61aa176e1bd7f45b4e6286f19c28743b4ed890e9
larads <dasilval.mariana@gmail.com>
2026-01-07 22:05:34 -0400

    feat: add sonner for toast notifications; integrate Toaster in App and enhance photo creation feedback in usePhoto hook

--- src/context/photos/hooks/use-photo.ts
+++ src/context/photos/hooks/use-photo.ts
@@ -2,7 +2,7 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { api, fetcher } from "../../../helpers/api";
 import type { Photo } from "../models/photo";
 import type { PhotoNewFormSchema } from "../../schema";
-
+import { toast } from "sonner";
 interface PhotoDetailResponse extends Photo {
   nextPhotoId?: string;
   previousPhotoId?: string;
@@ -18,7 +18,7 @@ export default function usePhoto(id?: string) {
   const queryClient = useQueryClient();
 
   async function createPhoto(payload: PhotoNewFormSchema) {
-    // eslint-disable-next-line no-useless-catch
+     
     try {
       const { data: photo } = await api.post<Photo>("/photos", {
         title: payload.title,
@@ -43,7 +43,9 @@ export default function usePhoto(id?: string) {
       }
 
       queryClient.invalidateQueries({ queryKey: ["photos"] });
+      toast.success("Foto criada com sucesso");
     } catch (error) {
+      toast.error("Erro ao criar foto");
       throw error;
     }
   }

bfafa65c250a590f9ef16bba65dadeba274b7088
larads <dasilval.mariana@gmail.com>
2026-01-07 20:31:46 -0400

    feat: integrate file upload component and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,12 @@ import Divider from "./components/divider";
 import InputText from "./components/input-text";
 import SearchIcon from "./assets/icons/search.svg?react";
 import InputCheckbox from "./components/input-checkbox";
+import InputSingleFile from "./components/input-single-file";
+import { useForm } from "react-hook-form";
 
 export default function App() {
+  const form = useForm();
+
   return (
     <div className="grid gap-7 p-6">
       <div className="flex gap-3">
@@ -58,6 +62,15 @@ export default function App() {
 
       <div>
         <InputCheckbox />
+      </div>
+
+      <div>
+        <InputSingleFile
+          allowedExtensions={["png", "jpg", "jpeg", "webp"]}
+          maxFileSizeInMB={50}
+          form={form}
+          {...form.register("file")}
+        />
       </div>
     </div>
   );
